b350d60deab831a62f5ecf8b0c59147f6350d8b1
plan <frostyplanet@gmail.com>
2013-11-09 05:33:18 +0000

    done

--- config.py
+++ config.py
@@ -9,8 +9,12 @@ log_dir = os.path.join (base_dir, "log")
 log_rotate_size = 20000
 log_backup_count = 3
 log_level = "DEBUG" 
-
 #end for log.py
+
+NGINX_CONF_PATH = os.path.join(base_dir, "nginx.conf")
+RUN_INV = 30
+NGINX_RELOAD_CMD = ["sudo", "/etc/init.d/nginx", "reload"]
+
 RUN_DIR = os.path.join (base_dir, "run")
 
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4 :
